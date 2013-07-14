d898b0f5e37013604296cd0af4dcdcb861384f2f
Arif Battal <battalarif@gmail.com>
2013-07-14 11:27:35 +0000

    Index.html from disk

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var kindle = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-    response.send('Hello World2!');
+  response.send(kindle.toString("utf-8") );
 });
 
 var port = process.env.PORT || 5000;
